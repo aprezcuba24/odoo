d3b3f67d5a53af77820d462a0785159ba78d2a81
Renier Ricardo Figueredo <aprezcuba24@gmail.com>
2026-06-05 20:59:28 +0000

    Telegram message

--- own_modules/order_bridge/listeners/order_created_listener.py
+++ own_modules/order_bridge/listeners/order_created_listener.py
@@ -1,7 +1,11 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
+from odoo.addons.order_bridge.utils.telegram_client import send_message
+from odoo.addons.order_bridge.utils.telegram_order_message import format_order_created_message
+
 
 def order_bridge_order_created(order, old_entity, new_entity):
     if order.order_bridge_origin != 'app':
         return
-    print(f'order_bridge: pedido creado desde APK id={order.id} ref={order.order_bridge_ref}')
+    text = format_order_created_message(order)
+    send_message(text, order_ref=order.order_bridge_ref)
