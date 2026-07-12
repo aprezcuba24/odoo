06ba9b8e8b26571f82d018452c27b789bbd47662
Renier Ricardo Figueredo <aprezcuba24@gmail.com>
2026-07-12 04:16:23 +0000

    filter by date and profit

--- own_modules/bi_analytics/models/product_sale_report.py
+++ own_modules/bi_analytics/models/product_sale_report.py
@@ -16,12 +16,14 @@ class BiProductSaleReport(models.Model):
     categ_id = fields.Many2one('product.category', string='Categoría de producto', readonly=True)
     company_id = fields.Many2one('res.company', string='Compañía', readonly=True)
     currency_id = fields.Many2one('res.currency', string='Moneda', readonly=True)
+    date_order = fields.Datetime(string='Fecha del pedido', readonly=True)
     qty_sold = fields.Float(string='Cantidad', readonly=True)
     sale_amount = fields.Monetary(string='Venta', readonly=True)
     cost_amount = fields.Monetary(string='Costo', readonly=True)
+    profit_amount = fields.Monetary(string='Ganancia', readonly=True)
 
     _depends = {
-        'sale.order': ['state', 'company_id'],
+        'sale.order': ['state', 'company_id', 'date_order'],
         'sale.order.line': [
             'product_id',
             'product_uom_qty',
@@ -35,21 +37,23 @@ class BiProductSaleReport(models.Model):
 
     @property
     def _table_query(self) -> SQL:
-        return SQL('%s %s %s %s', self._select(), self._from(), self._where(), self._group_by())
+        return SQL('%s %s %s', self._select(), self._from(), self._where())
 
     def _select(self) -> SQL:
         return SQL(
             """
                 SELECT
-                    MIN(l.id) AS id,
+                    l.id AS id,
                     l.product_id AS product_id,
                     p.product_tmpl_id AS product_tmpl_id,
                     t.categ_id AS categ_id,
                     s.company_id AS company_id,
                     c.currency_id AS currency_id,
-                    SUM(l.product_uom_qty) AS qty_sold,
-                    SUM(l.price_unit * l.product_uom_qty) AS sale_amount,
-                    SUM(l.purchase_price * l.product_uom_qty) AS cost_amount
+                    s.date_order AS date_order,
+                    l.product_uom_qty AS qty_sold,
+                    l.price_unit * l.product_uom_qty AS sale_amount,
+                    l.purchase_price * l.product_uom_qty AS cost_amount,
+                    (l.price_unit - l.purchase_price) * l.product_uom_qty AS profit_amount
             """,
         )
 
@@ -70,17 +74,5 @@ class BiProductSaleReport(models.Model):
                 WHERE s.state = 'sale'
                   AND l.display_type IS NULL
                   AND l.product_id IS NOT NULL
-            """,
-        )
-
-    def _group_by(self) -> SQL:
-        return SQL(
-            """
-                GROUP BY
-                    l.product_id,
-                    p.product_tmpl_id,
-                    t.categ_id,
-                    s.company_id,
-                    c.currency_id
             """,
         )
--- own_modules/bi_analytics/tests/test_product_sale_report.py
+++ own_modules/bi_analytics/tests/test_product_sale_report.py
@@ -1,5 +1,8 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
+from dateutil.relativedelta import relativedelta
+
+from odoo import fields
 from odoo.tests import tagged
 from odoo.tests.common import TransactionCase
 
@@ -17,16 +20,22 @@ class TestBiProductSaleReport(TransactionCase):
             'standard_price': 4.0,
         })
 
-    def test_product_sale_report_aggregates_confirmed_order(self):
+    def _create_confirmed_order(self, qty, price_unit, date_order=None):
         order = self.env['sale.order'].create({
             'partner_id': self.partner.id,
             'order_line': [(0, 0, {
                 'product_id': self.product.id,
-                'product_uom_qty': 2.0,
-                'price_unit': 10.0,
+                'product_uom_qty': qty,
+                'price_unit': price_unit,
             })],
         })
         order.action_confirm()
+        if date_order:
+            order.date_order = date_order
+        return order
+
+    def test_product_sale_report_aggregates_confirmed_order(self):
+        order = self._create_confirmed_order(2.0, 10.0)
         line = order.order_line
         report = self.env['bi.product.sale.report'].search([
             ('product_id', '=', self.product.id),
@@ -35,6 +44,7 @@ class TestBiProductSaleReport(TransactionCase):
         self.assertEqual(report.qty_sold, 2.0)
         self.assertAlmostEqual(report.sale_amount, 10.0 * 2.0)
         self.assertAlmostEqual(report.cost_amount, line.purchase_price * 2.0)
+        self.assertAlmostEqual(report.profit_amount, report.sale_amount - report.cost_amount)
 
     def test_product_sale_report_excludes_draft_orders(self):
         self.env['sale.order'].create({
@@ -49,3 +59,32 @@ class TestBiProductSaleReport(TransactionCase):
             ('product_id', '=', self.product.id),
         ])
         self.assertFalse(report)
+
+    def test_product_sale_report_filters_by_date(self):
+        now = fields.Datetime.now()
+        current_month_start = now.replace(day=1, hour=0, minute=0, second=0, microsecond=0)
+        previous_month = current_month_start - relativedelta(months=1)
+
+        self._create_confirmed_order(2.0, 10.0, date_order=now)
+        self._create_confirmed_order(5.0, 10.0, date_order=previous_month)
+
+        report_data = self.env['bi.product.sale.report'].read_group(
+            domain=[
+                ('product_id', '=', self.product.id),
+                ('date_order', '>=', current_month_start),
+                ('date_order', '<', current_month_start + relativedelta(months=1)),
+            ],
+            fields=['qty_sold', 'sale_amount'],
+            groupby=['product_id'],
+        )
+        self.assertEqual(len(report_data), 1)
+        self.assertEqual(report_data[0]['qty_sold'], 2.0)
+        self.assertAlmostEqual(report_data[0]['sale_amount'], 20.0)
+
+        all_data = self.env['bi.product.sale.report'].read_group(
+            domain=[('product_id', '=', self.product.id)],
+            fields=['qty_sold', 'sale_amount'],
+            groupby=['product_id'],
+        )
+        self.assertEqual(all_data[0]['qty_sold'], 7.0)
+        self.assertAlmostEqual(all_data[0]['sale_amount'], 70.0)
